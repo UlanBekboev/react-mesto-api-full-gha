70b246e422c6bf26e69956b7ab51f3800beb0b43
Ulan Bekboev <ubekboouulu@gmail.com>
2023-08-04 13:05:51 +0600

    "refactored errorLogger"

--- backend/app.js
+++ backend/app.js
@@ -22,10 +22,9 @@ mongoose.connect(DB, {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(requestLogger);
 
 app.use(cors);
-
-app.use(requestLogger);
 
 app.post('/signin', signIn, login);
 app.post('/signup', signUp, createUser);
