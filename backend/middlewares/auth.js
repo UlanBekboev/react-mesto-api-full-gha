5a549198282a2353f20cb5d20d2c54507564ebd3
Ulan Bekboev <ubekboouulu@gmail.com>
2023-09-25 02:14:37 +0600

    изменяет config.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,17 +1,14 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
 
-const { NODE_ENV, JWT_SECRET } = require('../utils/config');
+const { SECRET_STRING } = require('../utils/config');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(
-      token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-    );
+    payload = jwt.verify(token, SECRET_STRING);
   } catch (err) {
     return next(new UnauthorizedError('Необходима авторизация.'));
   }
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 
 const { // eslint-disable-next-line
-  NODE_ENV, JWT_SECRET, DB_HOST, PORT, SECRET,
+  NODE_ENV, JWT_SECRET, DB_HOST, PORT, SECRET
 } = process.env;
-
-const DEV_SECRET = '18aff85b4d9b2cf750c6e130ac8f7b2edb5d6dc590153b795f95e59cd0d396ee';
+const DEV_SECRET = 'dev-secret';
 const DEV_DB_HOST = 'mongodb://127.0.0.1:27017/mestodb';
 const DEV_PORT = 3000;
 
@@ -12,7 +11,7 @@ const DB = NODE_ENV === 'production' && DB_HOST ? DB_HOST : DEV_DB_HOST;
 
 const SERVER_PORT = NODE_ENV === 'production' && PORT ? PORT : DEV_PORT;
 
-const SECRET_STRING = NODE_ENV === 'production' && SECRET ? SECRET : DEV_SECRET;
+const SECRET_STRING = NODE_ENV === 'production' && JWT_SECRET ? JWT_SECRET : DEV_SECRET;
 
 module.exports = {
   DB,
